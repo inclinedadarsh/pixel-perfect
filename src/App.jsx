71bbc4ef8aa69d62591ab172174f7ee9f3528f92
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-04-04 02:04:53 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+
+function App() {
+    useEffect(() => {
+        window.location.href =
+            "https://www.figma.com/community/file/1357449136615593260/pixel-perfect-competition";
+    }, []);
+    return null;
+}
+
+export default App;
